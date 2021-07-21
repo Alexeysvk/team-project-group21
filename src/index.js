71a3f39ed6d4f21b7981fefb2b87e9358700e1bc
Alexeysvk <83663482+Alexeysvk@users.noreply.github.com>
2021-07-21 17:16:52 +0300

    try

--- src/index.js
+++ src/index.js
@@ -13,3 +13,28 @@ import './sass/main.scss';
     refs.menu.classList.toggle('is-open');
   }
 })();
+
+$(function () {
+  $('.rev_slider').slick({
+      arrows:false,
+  dots:true,
+  slidesToShow:1,
+  autoplay:false,
+  speed:1000,
+  autoplaySpeed:800,
+  responsive:[
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow:1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow:1  
+      }
+    }
+  ]
+});
+});
